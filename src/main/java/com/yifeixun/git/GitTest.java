4caaa7589ca11d45461a31945ab791b88e4d9324
tanshuqi <1650688519@qq.com>
2021-08-01 10:29:57 +0800

    first commit

---
+++ src/main/java/com/yifeixun/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yifeixun.git;
+
+/**
+ * @Author Tanshuqi
+ * @Date 2021/8/1 10:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
